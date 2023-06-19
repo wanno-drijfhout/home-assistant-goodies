be486ed896d3db77296b9ed3d0c2e69e8c190951
Wanno Drijfhout <github.com@wanno.eu>
2023-06-19 10:16:00 +0200

    Switch to apache

--- wordpress/Readme.md
+++ wordpress/Readme.md
@@ -6,3 +6,15 @@ This add-on deploys Wordpress on Home Assistant.
 
 - [x] MariaDB add-on must be installed
 - [x] Reverse proxy to take care of SSL and domain names
+
+## Deployment
+
+The add-on creates a folder `/share/wordpress` if it doesn't exist yet, and subsequently downloads and configures the latest version of WordPress.
+
+## Credits
+
+Inspiration from:
+
+- the [apache2 add-on](https://github.com/FaserF/hassio-addons/tree/master/apache2)
+- [WordPress recommendation on reverse proxying](https://wordpress.org/documentation/article/administration-over-ssl/)
+- [WordPress recommendation for apache2](https://wordpress.org/documentation/article/nginx/)
--- wordpress/run.sh
+++ wordpress/run.sh
@@ -1,3 +1,33 @@
 #!/usr/bin/with-contenv bashio
 
-echo "Hello word!"
+DocumentRoot=/share/wordpress
+echo "Document root: ${DocumentRoot}."
+if [ ! -d $DocumentRoot ]; then
+    mkdir -p $DocumentRoot
+    cd $DocumentRoot
+
+    echo "Downloading new WordPress installation..."
+    curl -L -s "https://wordpress.org/latest.tar.gz" \
+      | tar zxvf - --strip 1 -C $DocumentRoot
+
+    echo "Adapting configuration..."
+    cp /wp-config.php .
+    sed -i 's/database_name_here/(bashio::config "db_name")/g' /var/www/localhost/htdocs/wp-config.php
+    sed -i 's/username_here/(bashio::config "db_username")/g' /var/www/localhost/htdocs/wp-config.php
+    sed -i 's/password_here/(bashio::config "db_password")/g' /var/www/localhost/htdocs/wp-config.php
+    sed -i 's/localhost/core_mariadb:3306/g' /var/www/localhost/htdocs/wp-config.php
+    curl -s https://api.wordpress.org/secret-key/1.1/salt/ \
+       | sed -e '/\/\*\*#@+\*/,/\/\*\*#@-\*\//d' -e '/\/\*\*#@-\*\//r /dev/stdin' -i /var/www/localhost/htdocs/wp-config.php 
+    
+    echo "Configured."
+else
+    echo "Found existing folder"
+fi
+
+rm -rf /var/www/localhost/htdocs
+ln -s $DocumentRoot /var/www/localhost/htdocs
+
+mkdir /usr/lib/php81/modules/opcache
+
+echo "Starting Apache2..."
+exec /usr/sbin/httpd -D FOREGROUND
